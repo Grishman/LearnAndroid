f2d486db6a0de55e8ccc0ea254b0b6c9b5a181dd
Andrew Grishman <grishman4life@gmail.com>
2014-11-24 22:43:08 +0200

    Add UI to main
    added UI but tostring don work

--- FragTest/gen/com/sm/fragtest/R.java
+++ FragTest/gen/com/sm/fragtest/R.java
@@ -1653,19 +1653,20 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f050030;
         public static final int action_mode_bar_stub=0x7f05002f;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f050047;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000d;
         public static final int beginning=0x7f050012;
         public static final int bottom=0x7f050016;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000f;
+        public static final int countryText=0x7f050045;
+        public static final int countryView=0x7f050044;
         public static final int decor_content_parent=0x7f050031;
         public static final int default_activity_button=0x7f050026;
         public static final int dialog=0x7f050010;
         public static final int disableHome=0x7f05000a;
-        public static final int doURL=0x7f050044;
-        public static final int doURL2=0x7f050045;
+        public static final int doURL=0x7f05004a;
         public static final int dropdown=0x7f050011;
         public static final int edit_query=0x7f050035;
         public static final int end=0x7f050014;
@@ -1683,6 +1684,8 @@ containing a value of this type.
         public static final int never=0x7f05000b;
         public static final int none=0x7f050004;
         public static final int normal=0x7f050001;
+        public static final int paramsText=0x7f050049;
+        public static final int paramsView=0x7f050048;
         public static final int progress_circular=0x7f05001e;
         public static final int progress_horizontal=0x7f05001f;
         public static final int radio=0x7f05002d;
@@ -1701,10 +1704,12 @@ containing a value of this type.
         public static final int showCustom=0x7f050009;
         public static final int showHome=0x7f050006;
         public static final int showTitle=0x7f050008;
+        public static final int sourceText=0x7f050047;
+        public static final int sourceView=0x7f050046;
         public static final int split_action_bar=0x7f05001b;
         public static final int submit_area=0x7f05003e;
         public static final int tabMode=0x7f050003;
-        public static final int textView1=0x7f050046;
+        public static final int textView1=0x7f05004b;
         public static final int title=0x7f050029;
         public static final int top=0x7f050015;
         public static final int up=0x7f050018;
@@ -1763,9 +1768,10 @@ containing a value of this type.
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int activity_second=0x7f030018;
-        public static final int main_frag=0x7f030019;
-        public static final int result_frag=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int list_item=0x7f030019;
+        public static final int main_frag=0x7f03001a;
+        public static final int result_frag=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
@@ -1827,7 +1833,12 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
         public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000f;
+        public static final int countryT=0x7f0a0014;
         public static final int hello_world=0x7f0a0010;
+        public static final int paramsT=0x7f0a0016;
+        public static final int result=0x7f0a0013;
+        public static final int sourceT=0x7f0a0015;
+        public static final int startB=0x7f0a0017;
         public static final int title_activity_second=0x7f0a0012;
     }
     public static final class style {
--- FragTest/src/com/sm/fragtest/MainFrag.java
+++ FragTest/src/com/sm/fragtest/MainFrag.java
@@ -1,55 +1,95 @@
 package com.sm.fragtest;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainFrag extends Fragment implements OnClickListener  {
-
+public class MainFrag extends Fragment implements OnClickListener {
+	private static final String DEBUG_TAG = "MainFrag Activity";
+	private AutoCompleteTextView countryName;
+	private EditText country;
+	private EditText source;
+	private EditText params;
+	private StringBuilder strBuild;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		//return super.onCreateView(inflater, container, savedInstanceState);
-		View view = 
-				inflater.inflate(R.layout.main_frag, 
-				container, false);
-		
+		// return super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.main_frag, container, false);
 		Button doURL = (Button) view.findViewById(R.id.doURL);
-Button doURL2 = (Button) view.findViewById(R.id.doURL2);
 		doURL.setOnClickListener(this);
-		doURL2.setOnClickListener(this);
+
+		countryName = (AutoCompleteTextView) view.findViewById(R.id.countryText);
+
+		// Create an ArrayAdapter containing country names
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item, COUNTRIES);
+		countryName.setAdapter(adapter);
+		countryName.setOnItemClickListener(new OnItemClickListener() {
+
+			// Display a Toast Message when the user clicks on an item in the
+			// AutoCompleteTextView
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+//				Toast.makeText(getApplicationContext(),
+//						"Your choose: " + arg0.getAdapter().getItem(arg2),
+//						Toast.LENGTH_SHORT).show();
+
+			}
+		});
+		//country = (AutoCompleteTextView) view.findViewById(R.id.countryText);
+		String count = countryName.getText().toString();
+		source = (EditText) view.findViewById(R.id.sourceText);
+		params = (EditText) view.findViewById(R.id.paramsText);
+		strBuild = new StringBuilder();
+		strBuild.append("http://www.quandl.com/api/v1/datasets/");
+		strBuild.append(source.getText().toString());
+		strBuild.append("/");
+		strBuild.append(count);
+		strBuild.append("_");
+		strBuild.append(params.getText().toString());
 		
-				return view;
-							
+		return view;
+
 	}
+
 	int translateIdToIndex(int id) {
 		int index = -1;
 		switch (id) {
 		case R.id.doURL:
 			index = 1;
 			break;
-		case R.id.doURL2:
-			index = 2;
-			break;
-		
+//		case R.id.doURL2:
+//			index = 2;
+//			break;
+
 		}
 		return index;
 	}
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		int buttonIndex = translateIdToIndex(v.getId());
-       String str = new String("URLURLURL");
+		String str = new String("URLURLURL");
 		// Закоментируйте перед проверкой
 		OnSelectedButtonListener listener = (OnSelectedButtonListener) getActivity();
-		listener.doURLMagic(str);
+		listener.doURLMagic(strBuild.toString());
 
 		// Вспомогательный метод для получения индекса нажатой кнопки
 		Toast.makeText(getActivity(), String.valueOf(buttonIndex),
@@ -57,5 +97,26 @@ Button doURL2 = (Button) view.findViewById(R.id.doURL2);
 	}
 
 	
-
+	static final String[] COUNTRIES = new String[] { "AFG", "AGO", "ALB",
+		"ARE", "ARG", "ARM", "ATG", "AUS", "AUT", "AZE", "BDI", "BEL",
+		"BEN", "BFA", "BGD", "BGR", "BHR", "BHS", "BIH", "BLR", "BLZ",
+		"BOL", "BRA", "BRB", "BRN", "BTN", "BWA", "CAF", "CAN", "CHE",
+		"CHL", "CHN", "CIV", "CMR", "COD", "COG", "COL", "COM", "CPV",
+		"CRI", "CUB", "CYP", "CZE", "DEU", "DJI", "DMA", "DNK", "DOM",
+		"DZA", "ECU", "EGY", "ERI", "ESP", "EST", "ETH", "FIN", "FJI",
+		"FRA", "GAB", "GBR", "GEO", "GHA", "GIN", "GMB", "GNB", "GNQ",
+		"GRC", "GRD", "GTM", "GUY", "HKG", "HND", "HRV", "HTI", "HUN",
+		"IDN", "IND", "IRL", "IRN", "IRQ", "ISL", "ISR", "ITA", "JAM",
+		"JOR", "JPN", "KAZ", "KEN", "KGZ", "KHM", "KIR", "KNA", "KOR",
+		"KSV", "KWT", "LAO", "LBN", "LBR", "LBY", "LCA", "LKA", "LSO",
+		"LTU", "LUX", "LVA", "MAR", "MDA", "MDG", "MDV", "MEX", "MKD",
+		"MLI", "MLT", "MMR", "MNE", "MNG", "MOZ", "MRT", "MUS", "MWI",
+		"MYS", "NAM", "NER", "NGA", "NIC", "NLD", "NOR", "NPL", "NZL",
+		"OMN", "PAK", "PAN", "PER", "PHL", "PNG", "POL", "PRK", "PRT",
+		"PRY", "QAT", "ROU", "RUS", "RWA", "SAU", "SDN", "SEN", "SGP",
+		"SLB", "SLE", "SLV", "SMR", "SOM", "SRB", "SSD", "STP", "SUR",
+		"SVK", "SVN", "SWE", "SWZ", "SYC", "SYR", "TCD", "TGO", "THA",
+		"TJK", "TKM", "TLS", "TON", "TTO", "TUN", "TUR", "TUV", "TZA",
+		"UGA", "UKR", "URY", "USA", "UZB", "VCT", "VEN", "VNM", "VUT",
+		"WSM", "WSM", "ZAF", "ZMB", "ZWE" };
 }
